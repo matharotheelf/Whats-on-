b922d7ffb1bad3b9e0df81647f2c65fc337a33c7
matharotheelf <mrthomasjacoblawrence@hotmail.co.uk>
2019-01-25 14:01:09 +0000

    Hidden API KEY

--- scripts/main.js
+++ scripts/main.js
@@ -42,7 +42,8 @@ weatherButton.addEventListener('click', function () {
    var city = document.getElementById("cityname");
    var weatherDiv = document.getElementById("weatherDiv");
    var request = new XMLHttpRequest();
-   request.open('GET', "http://api.openweathermap.org/data/2.5/weather?q=" + city.value + "&APPID=9a282a7c672d2ce76ceed0ffe8323722", true);
+   var mykey = config.MY_KEY;
+   request.open('GET', "http://api.openweathermap.org/data/2.5/weather?q=" + city.value + "&APPID=" + mykey, true);
    request.onload = function () {
      var result = JSON.parse(this.response);
      weatherDiv.innerText = `${result.weather[0].description} ${result.main.temp_max - 273.15}`
